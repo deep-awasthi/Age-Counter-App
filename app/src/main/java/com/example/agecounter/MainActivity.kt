afdb35bb8ce9f4619d14bd163b76e8c89bf563b6
mad-skull <da.madskull@gmail.com>
2021-09-07 01:49:14 +0530

    date picker

--- app/src/main/java/com/example/agecounter/MainActivity.kt
+++ app/src/main/java/com/example/agecounter/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.example.agecounter
 
+
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.sql.Date
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {view ->
+            clickDatePicker(view)
+            Toast.makeText(this, "Button Works", Toast.LENGTH_LONG).show()
+        }
+    }
+
+fun clickDatePicker(view: View?) {
+
+    val myCalender = Calendar.getInstance();
+    val year = myCalender.get(Calendar.YEAR);
+    val month = myCalender.get(Calendar.MONTH);
+    val day = myCalender.get(Calendar.DAY_OF_MONTH);
+
+    DatePickerDialog(this,DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->  }
+        , year, month, day).show()
     }
 }
